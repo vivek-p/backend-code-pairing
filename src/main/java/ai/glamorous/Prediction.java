466fccbb90edd803841dfe4e442608e32f4257d2
RadwanNizam <radwan.nizam@glamorous.ai>
2022-11-11 09:48:40 +0000

    Initial commit

---
+++ src/main/java/ai/glamorous/Prediction.java
@@ -0,0 +1,11 @@
+package ai.glamorous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prediction {
+    public static void main(String[] args) {
+        SpringApplication.run(Prediction.class, args);
+    }
+}
